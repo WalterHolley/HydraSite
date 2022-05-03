3de9854afb89b85d70ab210393678f6f6019da58
jenPutnam <89960352+jenPutnam@users.noreply.github.com>
2022-05-03 02:21:10 -0500

    Fixed image sources

--- js/imageMapper.js
+++ js/imageMapper.js
@@ -11,11 +11,11 @@ function imageMap(move) {
 	var mapNum = 0;
 	switch(move) {
 		case 1: mapNum = 1;
-				imgStr = "<img src='battle_hydra"+ mapNum +".png' />";
+				imgStr = "<img src='img/battle_hydra"+ mapNum +".png' />";
 				document.getElementById("inputDiv").insertAdjacentHTML("afterBegin", imgStr);
 				break;
 		case 2: mapNum = 3;
-				imgStr = "<img src='battle_hydra"+ mapNum +".png' />";
+				imgStr = "<img src='img/battle_hydra"+ mapNum +".png' />";
 				document.getElementById("inputDiv").insertAdjacentHTML("afterBegin", imgStr);
 				break;
 	}
