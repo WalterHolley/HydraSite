a4194ce366410bb241eb9a38485d9975541491da
jenPutnam <89960352+jenPutnam@users.noreply.github.com>
2022-05-03 02:11:25 -0500

    Create imageMapper.js

---
+++ js/imageMapper.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+   Warrior Image Script
+   Author: Jennifer Putnam
+   Date: 5/3/2022
+*/
+
+// Insert User Character image for corresponding move
+
+function imageMap(move) {
+	var mapNum = 0;
+	switch(move) {
+		case 1: mapNum = 1;
+				imgStr = "<img src='battle_hydra"+ mapNum +".png' />";
+				document.getElementById("inputDiv").insertAdjacentHTML("afterBegin", imgStr);
+				break;
+		case 2: mapNum = 3;
+				imgStr = "<img src='battle_hydra"+ mapNum +".png' />";
+				document.getElementById("inputDiv").insertAdjacentHTML("afterBegin", imgStr);
+				break;
+	}
+}
